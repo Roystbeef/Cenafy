bd7fa6c00b776fa9a53fc9e4e7b1fc9cabc3cc28
Roy Fu <royfu@purdue.edu>
2015-10-23 01:11:51 -0400

    Refactor code and correct url to gif

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	origSpook();
+	getCenaed();
 });
 
 var generateHTML = function() {
-	var html = "<img src='https://raw.githubusercontent.com/Roystbeef/Spoookify/master/john_cena.gif' style='height: 100vh; width: 100vw'></img>";
+	var html = "<img src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/john_cena.gif' style='height: 100vh; width: 100vw'></img>";
 	html += "<audio id='scream' src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/john_cena.mp3'></audio>";
 	return html;
 };
@@ -12,18 +12,18 @@ var playAudio = function () {
 	document.getElementById('scream').play();
 };
 
-var spook = function() {
+var cenafy = function() {
 	var html = generateHTML();
 	$('body').html(html);
 	playAudio();
 	setTimeout(function() {
 		window.location.replace(document.URL);
-	}, 2000);
+	}, 20000);
 };
 
-var origSpook = function() {
+var getCenaed = function() {
 	var chance = Math.floor(Math.random() * 1000);
 	if(chance == 69) {
-		spook();
+		cenafy();
 	}
 };
