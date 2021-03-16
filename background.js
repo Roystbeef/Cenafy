d9a629e0bcf84c57df04a90b5a938840db44ae81
Roy Fu <rfu110@gmail.com>
2021-03-16 14:35:53 -0700

    Remove jQuery and remove tabs permission

--- background.js
+++ background.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-	var chance = Math.floor(Math.random() * 100);
-	if(chance == 69) {
-		cenafy();
-	}
-});
+const DELAY_IN_MS = 3000;
+const VIDEO_LENGTH_IN_MS = 20000;
 
-var generateHTML = function() {
-	var html = "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.webm' type='video/webm'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.mp4' type='video/mp4'></video>";
-	return html;
-};
+const VIDEO_HTML_STRING =
+  "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.webm' type='video/webm'><source src='https://raw.githubusercontent.com/Roystbeef/Cenafy/master/cena.mp4' type='video/mp4'></video>";
 
-var cenafy = function() {
-	setTimeout(function() { // Wait 3 seconds before cenafying
-		var html = generateHTML();
-		$('body').html(html);
-		document.body.style.backgroundColor = "black";
-		setTimeout(function() { // Wait 20 seconds for cena to finish
-			window.location.replace(document.URL);
-		}, 20000);
-	}, 3000);
-};
+function cenafy() {
+  setTimeout(() => {
+    const body = document.body;
+    body.innerHTML = VIDEO_HTML_STRING;
+    body.style.backgroundColor = "black";
+    setTimeout(() => {
+      // Wait 20 seconds for cena to finish
+      window.location.replace(document.URL);
+    }, VIDEO_LENGTH_IN_MS);
+  }, DELAY_IN_MS);
+}
+
+const chance = Math.floor(Math.random() * 100);
+if (chance === 69) {
+  cenafy();
+}
